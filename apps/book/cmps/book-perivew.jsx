83e950c42e0f04644fda69d2f644210378ba137a
shneor rebinovitz <shneor.333git config --global user.email shneor.333@gmail.com>
2022-07-04 20:13:54 +0300

    ...

--- apps/book/cmps/book-perivew.jsx
+++ apps/book/cmps/book-perivew.jsx
@@ -4,15 +4,16 @@ const { Link } = ReactRouterDOM;
 
 
 export function BookPreview({ book }) {
-  const{listPrice} =book
+  const { listPrice } = book
   return (
     <Link to={`/bookapp/${book.id}`} className="book-preview">
       <article >
         <div className="image-container">
           <img src={book.thumbnail} />
         </div>
+        <h4>{book.title.charAt(0).toUpperCase() + book.title.slice(1)}</h4>
         <h4>
-        {listPrice.isOnSale && (<img className='sale-img' src='../assets/img/sale.jpeg' />)}
+          {listPrice.isOnSale && (<img className='sale-img' src='../assets/img/sale.jpeg' />)}
           Price:{' '}
           {utilService.getCurrencySign(book.listPrice.currencyCode) + ' '}
           {book.listPrice.amount}
